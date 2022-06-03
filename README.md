9670986aa48264c49d81e108f3c44a2916a72e28
mvrius69 <xmariusro@gmail.com>
2022-06-03 16:17:24 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Frontend Mentor - QR code component
+
+![Design preview for the QR code component coding challenge](./design/desktop-preview.jpg)
+
+## Welcome! 👋
+
+I've done this challenge especially to try TailwindCSS.
+It's really nice to use it, and also easy to learn from the official website.
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px"
+    },
+    extend: {
+      colors: {
+        white: "hsl(0, 0%, 100%)",
+        lightGray: "hsl(212, 45%, 89%)",
+        grayishBlue: "hsl(220, 15%, 55%)",
+        darkBlue: "hsl(218, 44%, 22%)",
+      },
+    },
+  },
+  plugins: [],
+}
